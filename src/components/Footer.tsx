684d41d1ab8a74877f00af407f3a61bd9977c380
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-21 10:14:38 -0300

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <Image
+        src={"/assets/dogs-footer.svg"}
+        alt="Dogs"
+        width={28}
+        height={22}
+      />
+      <p>Dogs - Desenvolvido por Paulo Victor, Alguns direitos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
