837b01f4b1d1827ab4dfd86d478678ab8271e69e
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-21 09:47:28 -0300

    initial settings

---
+++ src/functions/fonts.ts
@@ -0,0 +1,8 @@
+import { Spectral } from "next/font/google";
+
+export const type_secound = Spectral({
+  weight: ["700"],
+  subsets: ["latin"],
+  variable: "--type-secound-spectral",
+  display: "swap",
+});
