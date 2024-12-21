77dca451b2ac7d4e653e97e102e02f590e3573b3
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-21 10:48:51 -0300

    feat: add header, and footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { type_secound } from "@/functions/fonts";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
@@ -14,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={type_secound.variable}>{children}</body>
+      <body className={type_secound.variable}>
+        <div className="App">
+          <Header />
+          <main className="AppBody">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
