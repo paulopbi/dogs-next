d01ff7e87368d03ae8c0ebfb41656e64b60ba58b
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-23 13:51:13 -0300

    chore: revalidate the page cache to update every 10sec

--- src/actions/photos-get.ts
+++ src/actions/photos-get.ts
@@ -14,7 +14,13 @@ export type Photo = {
 
 export default async function photosGet() {
   const request = await fetch(
-    "https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0"
+    "https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0",
+    {
+      next: {
+        revalidate: 10,
+        tags: ["photos"],
+      },
+    }
   );
   const data = (await request.json()) as Photo[];
   return data;
