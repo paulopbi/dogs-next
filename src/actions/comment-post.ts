0ae65a88b3f8658217e53528a9a907cd9e4693d5
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-24 09:17:14 -0300

    feat: add commentPost action for posting user comments

---
+++ src/actions/comment-post.ts
@@ -0,0 +1,32 @@
+"use server";
+import { COMMENT_POST } from "@/functions/api";
+import { cookies } from "next/headers";
+import apiError from "@/functions/api-error";
+import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default async function commentPost(state: {}, formdata: FormData) {
+  const token = cookies().get("token")?.value;
+  const comment = formdata.get("comment") as string | null;
+  const id = formdata.get("id") as string | null;
+
+  try {
+    if (!token || !comment || !id) throw new Error("Preencha todos os dados.");
+
+    const { url } = COMMENT_POST(id);
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      body: formdata,
+    });
+
+    if (!response.ok) throw new Error("Email ou usuário já cadastrado.");
+    const data = (await response.json()) as Comment;
+    revalidateTag("comment");
+    return { data, ok: true, error: "" };
+  } catch (error: unknown) {
+    return apiError(error);
+  }
+}
