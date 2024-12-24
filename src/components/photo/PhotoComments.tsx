cf65d0125e114f2bac13e95d266a6d0ab90ba533
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-24 09:16:35 -0300

    feat: add PhotoComments and PhotoCommentsForm components for user comments functionality

---
+++ src/components/photo/PhotoComments.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import PhotoCommentsForm from "@/components/photo/PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
+import { useUser } from "@/context/userContext";
+import { Comment } from "@/actions/photo-get";
+
+const PhotoComments = (props: {
+  single: boolean;
+  id: number;
+  comments: Comment[];
+}) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef<HTMLUListElement>(null);
+  const { user } = useUser();
+
+  React.useEffect(() => {
+    if (commentsSection.current) {
+      commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+    }
+  }, [comments]);
+
+  return (
+    <>
+      <ul
+        ref={commentsSection}
+        className={`${styles.comments} ${props.single ? styles.single : ""}`}
+      >
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {user && (
+        <PhotoCommentsForm
+          single={props.single}
+          id={props.id}
+          setComments={setComments}
+        />
+      )}
+    </>
+  );
+};
+
+export default PhotoComments;
