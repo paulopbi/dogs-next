fd8faa8fba459fe4cb431e143121f076a714f0cc
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-24 09:14:58 -0300

    feat: add PhotoContent component for displaying photo details and comments

---
+++ src/components/photo/PhotoContent.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useUser } from "@/context/userContext";
+import { PhotoData } from "@/actions/photo-get";
+import React from "react";
+import PhotoComments from "./PhotoComments";
+import styles from "./PhotoContent.module.css";
+import PhotoDelete from "./PhotoDelete";
+import Link from "next/link";
+import Image from "next/image";
+
+const PhotoContent = ({
+  data,
+  single,
+}: {
+  data: PhotoData;
+  single: boolean;
+}) => {
+  const { user } = useUser();
+  const { photo, comments } = data;
+
+  return (
+    <div className={`${styles.photo} ${single ? styles.single : ""}`}>
+      <div className={styles.img}>
+        <Image src={photo.src} alt={photo.title} width={1000} height={1000} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p className={styles.author}>
+            {user && user.username === photo.author ? (
+              <PhotoDelete id={String(photo.id)} />
+            ) : (
+              <Link href={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link href={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
+        </div>
+      </div>
+      <PhotoComments single={single} id={photo.id} comments={comments} />
+    </div>
+  );
+};
+
+export default PhotoContent;
