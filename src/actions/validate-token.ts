0335c899a3e158364d3e8f95bbc43c5c827df1d1
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-25 21:48:42 -0300

    feat: add validateToken action

---
+++ src/actions/validate-token.ts
@@ -0,0 +1,23 @@
+"use server";
+import { STATS_GET, TOKEN_POST, TOKEN_VALIDATE_POST } from "@/functions/api";
+import { cookies } from "next/headers";
+import apiError from "@/functions/api-error";
+
+export default async function validateToken() {
+  try {
+    const token = cookies().get("token")?.value;
+    if (!token) throw new Error("Acesso negado.");
+    const { url } = TOKEN_VALIDATE_POST();
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    if (!response.ok) throw new Error("Erro ao validar token.");
+    const data = await response.json();
+    return { data, ok: true, error: "" };
+  } catch (error) {
+    return apiError(error);
+  }
+}
